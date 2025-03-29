c3c4881aa59149d0280dae6f8ab455ecd656fcd6
RazGoelman <goelmanraz@gmail.com>
2025-03-29 22:50:48 +0300

    Add data transmission layer with FIFO buffering
    Implemented the transmission layer for secure data transfer.
    Includes a FIFO buffer between the encryption block and the UART
    transmitter to ensure reliable and lossless communication. The
    FIFO is configured for 18-bit wide data and 1024-depth to support
    flow control. This module outputs  and
    based on FIFO content. Ready signal () prevents
    overflow and data loss during UART delays.

---
+++ CipherComm_FPGA.srcs/sources_1/new/data_transmission.vhd
@@ -0,0 +1,74 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/29/2025 08:48:43 PM
+-- Design Name: 
+-- Module Name: data_transmission - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+--architecture
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity data_transmission is
+  port (
+    clk           : in  std_logic;
+    rst           : in  std_logic;
+    fifo_empty    : in  std_logic;
+    fifo_dout     : in  std_logic_vector(17 downto 0);
+    fifo_rd_en    : out std_logic;
+    tx_data_out   : out std_logic_vector(7 downto 0);
+    tx_valid_out  : out std_logic
+  );
+end entity;
+
+architecture Behavioral of data_transmission is
+  signal rd_en_int : std_logic := '0';
+
+begin
+ -- read from FIFO
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        rd_en_int     <= '0';
+        tx_data_out   <= (others => '0');
+        tx_valid_out  <= '0';
+      else
+        -- Check if FIFO not empty
+        if fifo_empty = '0' and fifo_dout(8) = '1' then
+          rd_en_int     <= '1';  -- trigger read
+          tx_data_out   <= fifo_dout(7 downto 0);
+          tx_valid_out  <= fifo_dout(8);  -- 9th bit as valid flag
+        else
+          rd_en_int     <= '0';
+          tx_valid_out  <= '0';
+        end if;
+      end if;
+    end if;
+  end process;
+
+  fifo_rd_en <= rd_en_int;
+
+end Behavioral;
